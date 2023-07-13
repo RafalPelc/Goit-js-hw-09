0f0ecee1490ca79966b7f5d2dadfb5a75f575850
RafalPelc <rafal.pelc2211@gmail.com>
2023-07-13 21:33:29 +0200

    HW9 the final touch

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,56 +1,42 @@
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', readUserInput);
 
-function handleSubmit(event) {
+function readUserInput(event) {
   event.preventDefault();
+  const firstDelay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const promiseCounter = Number(amountInput.value);
 
-  const delayInput = document.querySelector('[name="delay"]');
-  const stepInput = document.querySelector('[name="step"]');
-  const amountInput = document.querySelector('[name="amount"]');
-
-  const delay = parseInt(delayInput.value);
-  const step = parseInt(stepInput.value);
-  const amount = parseInt(amountInput.value);
-
-  createPromises(delay, step, amount)
-    .then(results => {
-      results.forEach(result => {
-        if (result.shouldResolve) {
-          Notiflix.Notify.success(
-            `✅ Fulfilled promise ${result.position} in ${result.delay}ms`
-          );
-        } else {
-          Notiflix.Notify.failure(
-            `❌ Rejected promise ${result.position} in ${result.delay}ms`
-          );
-        }
+  for (let i = 0; i < promiseCounter; i += 1) {
+    createPromise(i + 1, firstDelay + i * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
       });
-    })
-    .catch(error => {
-      console.error('Błąd:', error);
-    });
-}
-
-function createPromises(firstDelay, step, amount) {
-  let currentDelay = firstDelay;
-  let promises = [];
-
-  for (let i = 1; i <= amount; i++) {
-    promises.push(createPromise(i, currentDelay));
-    currentDelay += step;
   }
-
-  return Promise.all(promises);
 }
 
-const createPromise = (position, delay) => {
-  return new Promise(resolve => {
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
-      resolve({ position, delay, shouldResolve });
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
     }, delay);
   });
-};
+}
